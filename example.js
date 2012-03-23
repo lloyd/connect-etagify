114a02462a4ed0bbcf88a00522b78a13fe58fb5a
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-03-23 11:42:45 -0600

    write a pile of unit tests

--- example.js
+++ example.js
@@ -2,9 +2,28 @@ var app = require('express').createServer();
 
 app.use(require('./etagify.js')());
 
+// this is an example route that constructs and serves a non-static but
+// cachable page
 app.get('/page', function(req, res) {
+  // apply cache headers - allow caching but force revalidation all
+  // the time
+  res.setHeader('Cache-Control', 'public, max-age=0');  
+  
+  // use ETags
   res.etagify();
+
   res.send("pulitzer prize. bam.");
 });
 
-app.listen(8080);
+// this is and example route that is a rest api request
+app.get('/api', function(req, res) {
+  // for api calls, don't use etags and don't allow client to cache
+  // results.
+  res.setHeader('Cache-Control', 'no-cache, max-age=0');
+  res.send("no etagify here, because the results aren't cachable.");
+});
+
+app.listen(process.env['PORT'] || 8080, '127.0.0.1');
+
+// ignore me.  this is so tests can use this little server programatically.
+exports.app = app;
---
+++ tests.js
@@ -0,0 +1,117 @@
+const
+should = require('should'),
+http = require('http');
+
+process.env['PORT'] = 0;
+var app = require('./example.js').app;
+var port;
+
+describe('the test server', function() {
+  it('should bind an ephemeral port', function() {
+    port = app.address().port;
+    (port).should.not.equal(0);
+  });
+
+  it('should have good default 404 behavior', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/404'
+    }, function(res) {
+      res.statusCode.should.equal(404);
+      Object.keys(res.headers).should.not.include('etag');
+      done();
+    });
+  });
+});
+
+describe('an etagified resource', function() {
+  it('first request shouldnt have etag', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/page'
+    }, function(res) {
+      res.statusCode.should.equal(200);
+      Object.keys(res.headers).should.not.include('etag');
+      done();
+    });
+  });
+
+  var etag;
+
+  it('second request should have etag', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/page'
+    }, function(res) {
+      res.statusCode.should.equal(200);
+      Object.keys(res.headers).should.include('etag');
+      etag = res.headers['etag'];
+      done();
+    });
+  });
+
+  it('if-none-match header should trigger 304', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/page',
+      headers: {
+        'If-None-Match': etag
+      }
+    }, function(res) {
+      res.statusCode.should.equal(304);
+      Object.keys(res.headers).should.not.include('etag');
+      done();
+    });
+  });
+
+  it('should ignore get data (dear reader, dont use etagify here)', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/page?foo=bar',
+      headers: {
+        'If-None-Match': etag
+      }
+    }, function(res) {
+      res.statusCode.should.equal(304);
+      Object.keys(res.headers).should.not.include('etag');
+      done();
+    });
+  });
+});
+
+
+describe('a non-etagified resource', function() {
+  it('first request shouldnt have etag', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/api'
+    }, function(res) {
+      res.statusCode.should.equal(200);
+      Object.keys(res.headers).should.not.include('etag');
+      done();
+    });
+  });
+
+  var etag;
+
+  it('second request STILL shouldn\'t have etag', function(done) {
+    http.get({
+      host: '127.0.0.1',
+      port: port,
+      path: '/api'
+    }, function(res) {
+      res.statusCode.should.equal(200);
+      Object.keys(res.headers).should.not.include('etag');
+      etag = res.headers['etag'];
+      done();
+    });
+  });
+});
+
+
